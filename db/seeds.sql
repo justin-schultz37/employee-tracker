8f8dda55efcf274eee66540f34ad58842f4fb799
birdslisten <justin.schultz37@gmail.com>
2023-10-18 17:08:18 -0700

    added SQL queries to switch statements for the VIEW ALL options

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,23 @@
 INSERT INTO department (dept_name)
 VALUES
-("")
+("Engineering"),
+("Sales"),
+("Onboarding"),
+("Customer Support"),
+("Finance");
 
-INSERT INTO roles (title, salary, dept_id)
+INSERT INTO roles (title, salary)
 VALUES
-("")
+("Chief Technical Officer", 200000),
+("Chief Revenue Officer", 210000),
+("Chief Operating Officer", 170000),
+("Chief Client Experience", 145000),
+("Chief Financial Officer", 200000);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, manager_id)
 VALUES
-("")
+("Justin", "Schultz", 10),
+("Jack", "Sanogo", 20),
+("Arlin", "Lemmon", 30),
+("Juanda", "Pool", 40),
+("Jaime", "George", 50);
--- index.js
+++ index.js
@@ -2,29 +2,139 @@ const hide = require('hide-secrets');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'employee_db'
-    }
-);
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+});
 
-inquirer
-    .prompt(
-        {
+function startInquirer() {
+    inquirer
+        .prompt({
             type: 'list',
             message: 'What would you like to do?',
             name: 'start',
             choices: [
-                'View all Employees',
                 'View all Departments',
+                'View all Employees',
                 'View all Roles',
                 'Add Employee',
                 'Add Department',
                 'Add Role',
                 'Update Employee Role'
             ]
-
         })
+        .then((answer) => {
+            switch (answer.start) {
+                case 'View all Departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all Employees':
+                    viewAllEmployee();
+                    break;
+                case 'View all Roles':
+                    viewAllRoles();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                    break;
+            }
+        });
+}
+
+function viewAllDepartments() {
+    const query = `SELECT * FROM department`;
+    db.query(query, (error, res) => {
+        if (error) throw error;
+        console.table(res);
+        startInquirer();
+    });
+}
+
+function viewAllEmployee() {
+    const query =
+        `SELECT
+            employees.id AS employee_id,
+            employees.first_name,
+            employees.last_name,
+            roles.title AS job_title,
+            roles.salary
+        FROM
+            employees
+        JOIN
+            roles
+        ON
+            employees.role_id = roles.id;
+        `;
+    db.query(query, (error, res) => {
+        if (error) throw error;
+        console.table(res);
+        startInquirer();
+    });
+}
+
+function viewAllRoles() {
+    const query =
+        `SELECT
+            roles.id AS role_id,
+            roles.title AS job_title,
+            department.dept_name AS department,
+            roles.salary
+        FROM
+            roles
+        JOIN
+            department
+        ON
+            roles.dept_id = department.id;`;
+    db.query(query, (error, res) => {
+        if (error) throw error;
+        console.table(res);
+        startInquirer();
+    });
+}
+
+function addEmployee() {
+    const query = `SELECT * FROM department`;
+    db.query(query, (error, res) => {
+        if (error) throw error;
+        console.table(res);
+        startInquirer();
+    });
+}
+
+function addDepartment() {
+    const query = `SELECT * FROM department`;
+    db.query(query, (error, res) => {
+        if (error) throw error;
+        console.table(res);
+        startInquirer();
+    });
+}
+
+function addRole() {
+    const query = `SELECT * FROM department`;
+    db.query(query, (error, res) => {
+        if (error) throw error;
+        console.table(res);
+        startInquirer();
+    });
+}
+
+function updateEmployeeRole() {
+    const query = `SELECT * FROM department`;
+    db.query(query, (error, res) => {
+        if (error) throw error;
+        console.table(res);
+        startInquirer();
+    });
+}
