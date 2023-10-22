e08b8234ebb30a6b0fccbe4f57bb69158717fed3
birdslisten <justin.schultz37@gmail.com>
2023-10-22 13:42:22 -0700

    fixed error with seed data not populating

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,25 @@
 INSERT INTO department (dept_name)
 VALUES
-("Engineering"),
-("Sales"),
-("Onboarding"),
-("Customer Support"),
-("Finance");
+    ("Engineering"),
+    ("Sales"),
+    ("Onboarding"),
+    ("Customer Support"),
+    ("Finance");
 
-INSERT INTO roles (title, salary)
+-- Insert seed data into the 'roles' table
+INSERT INTO roles (id, title, salary, dept_id)
 VALUES
-("Chief Technical Officer", 200000),
-("Chief Revenue Officer", 210000),
-("Chief Operating Officer", 170000),
-("Chief Client Experience", 145000),
-("Chief Financial Officer", 200000);
+    (1, "Chief Technical Officer", 200000, 1),
+    (2, "Chief Revenue Officer", 210000, 2),
+    (3, "Chief Operating Officer", 170000, 3),
+    (4, "Chief Client Experience", 145000, 4),
+    (5, "Chief Financial Officer", 200000, 5);
 
-INSERT INTO employees (first_name, last_name, manager_id)
+-- Insert seed data into the 'employees' table
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-("Justin", "Schultz", 10),
-("Jack", "Sanogo", 20),
-("Arlin", "Lemmon", 30),
-("Juanda", "Pool", 40),
-("Jaime", "George", 50);
+    ("Justin", "Schultz", 1, 100),
+    ("Jack", "Sanogo", 2, 200),
+    ("Arlin", "Lemmon", 3, 300),
+    ("Juanda", "Pool", 4, 400),
+    ("Jaime", "George", 5, 500);
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'employee_db'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
